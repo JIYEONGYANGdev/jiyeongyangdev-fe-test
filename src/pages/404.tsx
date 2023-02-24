c49b5f6fc0c69ece9af10168f48508ddd8bb9c4e
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2023-02-24 18:21:17 +0900

    Custom Error Component ('/404')

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/Header';
+
+function Custom404() {
+  return (
+    <>
+      <Header />
+      <ErrorMessage>404 - Page Not Found</ErrorMessage>
+    </>
+  );
+}
+
+export default Custom404;
+
+const ErrorMessage = styled.h1`
+  text-align: center;
+`;
