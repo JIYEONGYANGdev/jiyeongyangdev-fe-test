d6c4cde0e9ecd59e9a9294403fda6b62f345e505
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2023-02-25 21:11:46 +0900

    Update useLoginService.tsx

--- src/service/useLoginService.tsx
+++ src/service/useLoginService.tsx
@@ -13,6 +13,8 @@ export const useGetUserInfo = (params: { userId: string }) =>
   useQuery(
     [GET_USER_INFO, params],
     async () => {
+      const { userId } = params
+
       if (!params.userId) return null
       const response = await axios.get(`https://api.sixshop.com/users/${userId}`)
 
